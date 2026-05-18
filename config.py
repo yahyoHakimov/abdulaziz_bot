4f3c6f663a5fb52eb2b7dc872654a154e8dab470
yahyoHakim <hakimovyahyo7@gmail.com>
2026-05-18 19:01:17 +0500

    feat: add 10 day interval option

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ DB_PATH: str = os.getenv("DB_PATH", "barber.db")
 MORNING_HOUR: int = int(os.getenv("MORNING_HOUR", "8"))
 EVENING_HOUR: int = int(os.getenv("EVENING_HOUR", "23"))
 
-INTERVALS = [15, 20, 25, 30]
+INTERVALS = [10, 15, 20, 25, 30]
 ADMIN_IDS: set[int] = {int(i.strip()) for i in os.environ["ADMIN_IDS"].split(",")}
 ADMIN_NAME: str = os.getenv("ADMIN_NAME", "Admin")
